afd593751fc4b41679d8c15605b2118a25fbea2f
Tran Phuc <phuc.tranngoc@vti.com.vn>
2020-08-19 09:08:48 +0700

    Change config mail server from application.properties to MailServerConfig. Create template html.

--- src/main/java/com/mailstarter/config/MailServerConfig.java
+++ src/main/java/com/mailstarter/config/MailServerConfig.java
@@ -16,23 +16,23 @@ import java.util.Properties;
 @Configuration
 public class MailServerConfig {
 
-//    @Bean
-//    public JavaMailSender getJavaMailSender() {
-//        JavaMailSenderImpl sender = new JavaMailSenderImpl();
-//        //Set host and port for Gmail
-//        sender.setHost("smtp.gmail.com");
-//        sender.setPort(587);
-//
-//        //Set username and password for MailSender
-//        sender.setUsername(MailInformation.USERNAME);
-//        sender.setPassword(MailInformation.PASSWORD);
-//
-//        Properties properties = sender.getJavaMailProperties();
-//        properties.put("mail.transport.protocol", "smtp");
-//        properties.put("mail.smtp.auth", "true");
-//        properties.put("mail.smtp.starttls.enable", "true");
-//        properties.put("mail.debug", "true");
-//
-//        return sender;
-//    }
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        //Set host and port for Gmail
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
+
+        //Set username and password for MailSender
+        sender.setUsername(SenderInformation.USERNAME);
+        sender.setPassword(SenderInformation.PASSWORD);
+
+        Properties properties = sender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return sender;
+    }
 }
--- src/main/java/com/mailstarter/config/SenderInformation.java
+++ src/main/java/com/mailstarter/config/SenderInformation.java
@@ -7,7 +7,8 @@ package com.mailstarter.config;
 public class SenderInformation {
     public static final String USERNAME = "phuc.tranngoc@vti.com.vn";
 
+    public static final String PASSWORD = "Ptr@1234";
+
     public static final String SIGNATURE = "Spring Mail Demo";
-//    public static final String PASSWORD = "31101998";
 
 }
